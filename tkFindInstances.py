4dcefe67db53221897cc84eb23293eaaf730b619
Thomas Kutschera <59307048+Gitte12345@users.noreply.github.com>
2020-01-28 22:44:03 +0100

    Update tkFindInstances.py

--- tkFindInstances.py
+++ tkFindInstances.py
@@ -2,15 +2,18 @@ import maya.cmds as cmds
 from functools import partial  
 import thomas as tk
 
-global INSTANCERLIST
-INSTANCERLIST = []
+global instancerList
+global referenceList
+global referencePath
+instancerList = []
+referenceList = []
 
 def tkFindInstances(*args):
 	cmds.select('INSTANCES')
 	tk.selectLevel('INSTANCES', 2, 0)
 	curSel = cmds.ls(sl=1, l=1)
 
-	# print curSel
+	mainLocGrp = cmds.group(em=1, n='mainLocGrp')
 
 	for sel in curSel:
 		children 	= cmds.listRelatives(sel, f=1, c=1)
@@ -18,12 +21,30 @@ def tkFindInstances(*args):
 
 		if len(parents) > 1:
 			for daddy in parents:
-				if daddy not in INSTANCERLIST:
-					INSTANCERLIST.append(daddy)
+				isRef = cmds.referenceQuery(daddy, isNodeReferenced=1)
+				if isRef == 1: 
+					referencePath = cmds.referenceQuery(daddy, filename=1)
+					if referencePath not in referenceList:
+						referenceList.append(referencePath)
+				
+			for daddy in parents:
+				loc = cmds.spaceLocator()
+				cmds.addAttr(loc[0], ln='path', dt='string')
+				# cmds.addAttr(loc + '.path', e=1)
 
-	# return INSTANCERLIST
-	print 'INSTANCERLIST:'
-	for inst in INSTANCERLIST:
-		print inst
+				cmds.setAttr(str(loc[0]) + '.path', referencePath, type='string')
+				cmds.parent(loc, mainLocGrp)
+
+
+
+
+
+
+
+
+
+
+
+
 
 tkFindInstances()
