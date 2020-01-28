5bd3d3e396097fbde229bedd0f692cbb234d724d
Thomas Kutschera <59307048+Gitte12345@users.noreply.github.com>
2020-01-28 22:02:19 +0100

    Update tkFindInstances.py

--- tkFindInstances.py
+++ tkFindInstances.py
@@ -1,13 +1,29 @@
 import maya.cmds as cmds
 from functools import partial  
+import thomas as tk
+
+global INSTANCERLIST
+INSTANCERLIST = []
 
 def tkFindInstances(*args):
-	instancerList = []
-	curSel = cmds.ls(sl=1)
-	for sel in curSel:
-		children 	= cmds.listRelatives(sel, fp=1, c=1)
-		parents 	= cmds.listRelatives(sel, fp=1, ap=1)
-		if len(parents) > 1:
-			instancerList.append(sel) 
+	cmds.select('INSTANCES')
+	tk.selectLevel('INSTANCES', 2, 0)
+	curSel = cmds.ls(sl=1, l=1)
 
-	return instacerList
+	# print curSel
+
+	for sel in curSel:
+		children 	= cmds.listRelatives(sel, f=1, c=1)
+		parents 	= cmds.listRelatives(sel, f=1, ap=1)
+
+		if len(parents) > 1:
+			for daddy in parents:
+				if daddy not in INSTANCERLIST:
+					INSTANCERLIST.append(daddy)
+
+	# return INSTANCERLIST
+	print 'INSTANCERLIST:'
+	for inst in INSTANCERLIST:
+		print inst
+
+tkFindInstances()
